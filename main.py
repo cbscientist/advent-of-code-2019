8aa6e920c57e7215e40eeded2765ec531b315aac
claire salling <claire@cloudbakers.com>
2019-12-02 10:36:23 -0600

    Add solution for puzzle 1 of day 2. Add incomplete solution for puzzle 2 day 2 - WIP

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from day_01 import fuel
+from day_02 import gravityAssist
 
 
 if __name__=="__main__":
+
+    # Day 1 Processing
     masses = fuel.parse_input_file('day_01/input.txt')
     fuel_needed = sum([fuel.calculate_total_fuel(fuel.calculate_fuel(mass)) for mass in masses])
 
     print("This is the amount of fuel needed: {}".format(fuel_needed))
+
+    # Day 2 Processing
+    integers = gravityAssist.parse_input_file('day_02/input.txt')
+    new_integers = gravityAssist.initialize_values(integers)
+    final_integers = gravityAssist.intcode_processing(new_integers)
+
+    print("This is position 0 of the final integer list:{}".format(final_integers[0]))
+
+    integers = gravityAssist.parse_input_file('day_02/input.txt')
+    noun, verb = gravityAssist.get_initial_values(integers)
+
+    print("The noun is {} and the verb is {}".format(noun, verb))
