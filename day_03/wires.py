df22ebebf77844637a60ba76db08e0356f4fdfec
claire salling <claire@cloudbakers.com>
2019-12-03 14:29:40 -0600

    Add function to calculate the Manhattan Distance between two points.

--- day_03/wires.py
+++ day_03/wires.py
@@ -38,3 +38,35 @@ def parse_input_file(input_file_name):
     wire_paths = [wire_path.split(',') for wire_path in wire_paths]
 
     return wire_paths
+
+
+# origin is 0,0
+# r is +x
+# l is -x
+# u is +y
+# d is -y
+
+# Calculate the coordinates of each line segment
+# wire 1, line 1, x1,y1, x2,y2, horizontal or vertical
+# wire 1, line 2, x1y1 x2y2 horizontal or vertical
+# wire 2, line 1, x1y1, x2y2, horizontal or vertical
+# wire 2, line 2, x1y1, x2y2, horizontal or vertical
+
+# match wire 1 horizontal with wire 2 vertical
+# calculate intersection points
+# calculate manhattan distance (distance from origin of 0,0)
+
+# match wire 1 vertical with wire 2 vertical
+# calculate intersection points
+# calculate manhattan distance (distance from origin of 0,0)
+
+# get list of points & manhattan distances
+# find minimum manhattan distance
+# return associated coordinate
+
+
+def calc_manhattan_distance(point_1, point_2):
+    x_distance = abs(point_2[0] - point_1[0])
+    y_distance = abs(point_2[1] - point_1[1])
+
+    return x_distance + y_distance
